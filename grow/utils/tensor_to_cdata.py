e478649d204d1be59166e67bbf3ce322e8c2d1b6
Chris <cfusting@gmail.com>
2021-01-06 03:49:28 +0000

    Dont record links.

--- grow/utils/tensor_to_cdata.py
+++ grow/utils/tensor_to_cdata.py
@@ -43,7 +43,7 @@ def add_cdata_to_xml(C, x_size, y_size, z_size, file_path, record_history=True):
         history.set("replace", "VXA.Simulator.RecordHistory")
         etree.SubElement(history, "RecordStepSize").text = "250"
         etree.SubElement(history, "RecordVoxel").text = "1"
-        etree.SubElement(history, "RecordLink").text = "1"
+        etree.SubElement(history, "RecordLink").text = "0"
         etree.SubElement(history, "RecordFixedVoxels").text = "0"
 
     file_content = etree.tostring(VXD, pretty_print=True).decode("utf-8")
