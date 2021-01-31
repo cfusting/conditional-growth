c4715343154da0f08f84d4b77267747fb3897aeb
Chris <cfusting@gmail.com>
2021-01-31 22:43:49 +0000

    sim working. Need to fix tests next.

---
+++ tests/test_gridenv.py
@@ -0,0 +1,39 @@
+import ray
+import numpy as np
+from ray.rllib.agents.ppo import PPOTrainer
+from grow.env.tensor_environment import TensorGrowthEnvironment
+
+
+def test_optimize_grid():
+    ray.init()
+
+    config = {
+        "env": TensorGrowthEnvironment,
+        "env_config": {
+            "materials": (0, 1),
+            "num_timestep_features": 1,
+            "max_steps": 10,
+            "reward_interval": 1,
+            "max_voxels": 6,
+            "search_radius": 3,
+            "axiom_material": 1,
+            "reward_type": "max_volume",
+            "voxel_size": 0.01,
+        },
+        "vf_clip_param": 10**5,
+        "seed": np.random.randint(10**5),
+        "num_workers": 8,
+        "num_gpus": 1,
+        "num_gpus_per_worker": 0,
+        "num_envs_per_worker": 1,
+        "framework": "torch",
+        "monitor": True,
+    }
+
+    ray.tune.run(
+        PPOTrainer,
+        name="grid-test",
+        config=config,
+        stop={"timesteps_total": 1},
+        local_dir="/tmp"
+    )
