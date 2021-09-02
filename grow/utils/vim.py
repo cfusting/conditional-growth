80e75a1970ffae46185c5b1405a32e1d7f780d05
Chris <cfusting@gmail.com>
2021-09-02 08:30:13 -0700

    Make nice.

--- grow/utils/vim.py
+++ grow/utils/vim.py
@@ -1,7 +1,6 @@
 import torch
 from torch import nn
 from torch.optim import Adam
-import numpy as np
 
 
 class VariationInformationMaximization:
@@ -48,10 +47,16 @@ class VariationInformationMaximization:
         X[:, self.num_actions : self.num_actions + self.state_dimensions] = start_state
         if end_state:
             X[:, self.num_actions + self.state_dimensions :] = end_state
-        X[:, actions] = torch.ones(X.shape[0])
+
+        # Actions are (batch_size, 1)
+        actions = actions.squeeze()
+        for i in range(X.shape[0]):
+            X[i, actions[i]] = torch.ones(X.shape[0])
         return X
 
     def get_action_decoder_probability(self, start_state, end_state, actions=None):
+        # We begin with the null action to approximate q(a|s, s') ~ q(a|s, s', a0).
+        # That is in place of no action we use the null action.
         selected_actions = torch.full(start_state.shape[0], self.null_action)
         probabilities = torch.ones((start_state.shape[0], self.num_actions))
         for i in range(self.num_action_steps):
@@ -59,7 +64,12 @@ class VariationInformationMaximization:
                 X = self.get_feature_tensor(selected_actions, start_state, end_state)
             else:
                 X = self.get_feature_tensor(actions[..., i], start_state, end_state)
+
+            # In: (batch_size, num_actions + z_size * 2)
+            # IE actions plus start and end states.
+            # Out: (batch_size, num_actions) each probability per action.
             y = self.action_decoder(X)
+            # This is *not* in place to respect the operation graph for backpropogation.
             probabilities = probabilities * y
             selected_actions = torch.argmax(probabilities, dim=1)
         y, _ = torch.max(probabilities, dim=1)
@@ -78,6 +88,8 @@ class VariationInformationMaximization:
             else:
                 X = self.get_feature_tensor(actions[..., i], start_state)
             probabilities[..., i] = self.source_action_state(X)
+            # Get selected actions for this iteration via indexing with i.
+            selected_actions = torch.argmax(probabilities[..., i], dim=1)
 
         return probabilities
 
@@ -94,18 +106,30 @@ class VariationInformationMaximization:
         probabilities = self.get_source_action_state_probabilities_per_action(
             start_state,
         )
-        actions = np.zeros(probabilities.shape)
-        for i in range(probabilities.shape[0]):
-            for j in range(probabilities.shape[2]):
-                sample_action = np.random.choice(
-                    self.num_actions,
-                    size=1,
-                    p=probabilities[i, :, j]
-                )
-                actions[i, :, j] = sample_action
+        # We prune off the last action (the result) and add the null action
+        # to the start as it's always the first action.
+        x = torch.full_like(probabilities, 1)
+        x[..., 0] = torch.full((x.shape[0], x.shape[1]), self.null_action)
+        x[..., 1:] = probabilities[..., :-1]
+
+        # Get one action per batch and action_step
+        actions = torch.zeros((x.shape[0], 1, x.shape[2]))
+        for i in range(x.shape[2]):
+            sample_actions = torch.multinomial(x[..., i], num_samples=1, replace=False)
+            actions[..., i] = sample_actions
         return actions
 
-    def optimize(self, start_state, end_state):
+    def step(self, start_state, end_state):
+        """Take a step toward optimizing the ELBO.
+
+        Note:
+            States are expected to be pre-processed from x to z via 3D convolution.
+            Failing to do so may make optimization much more difficult.
+
+        Parameters:
+            start_state: (batch_size, z_start)
+            end_state: (batch_size, z_end)
+        """
         action_decoder_probability = self.get_action_decoder_probability(
             start_state, end_state
         )
@@ -118,6 +142,7 @@ class VariationInformationMaximization:
         print(f"Action Decoder Loss: {action_decoder_loss}")
 
         # We use samples from the source distribution to optimize.
+        # (batch_size, z_start) -> (batch_size, 1, num_action_steps).
         action_sequence_sample = self.sample_source_distribution(start_state)
 
         # Minimize the loss of the approximation to the source distribution.
@@ -129,9 +154,9 @@ class VariationInformationMaximization:
             start_state, end_state, actions=action_sequence_sample
         )
         scalar = self.source_state(start_state)
-        source_loss = self.explortation_loss(
-            self.beta * torch.log(action_decoder_probability)
-            - (torch.log(source_action_state_probability) + scalar)
+        source_loss = self.source_loss(
+            self.beta * torch.log(action_decoder_probability),
+            (torch.log(source_action_state_probability) + scalar),
         )
         source_loss.backward()
         self.source_optimizer.step()
