ee272b0cb8ebb95a14ba64472807080f850bd731
chris <cfusting@gmail.com>
2022-11-13 15:58:56 -0500

    Fixing

--- Dockerfile
+++ Dockerfile
@@ -1,16 +1,7 @@
 # docker run -it --rm --gpus all --network host growth python run_configurations/minecraft/run.py
-FROM rayproject/ray-ml:latest-gpu
+FROM rayproject/ray-ml:2.1.0-gpu
 
 WORKDIR /ray
-
-# RUN sudo apt-get update
-# RUN sudo apt-get remove -y --purge cmake
-# RUN sudo apt-get install -y git libboost-all-dev wget screen vim
-# RUN sudo apt-get install -y ffmpeg libsm6 libxext6 rsync libgl1-mesa-dev xvfb
-# RUN pip install pyvista==0.27.4 dm-tree==0.1.5 lxml==4.6.2 pytest==6.2.1 matplotlib==3.3.3 vtk==8.1.2
-
-# pip version has recording bug.
-# RUN pip uninstall -y gym && git clone https://github.com/openai/gym.git && cd gym && pip install -e . && cd ..
 
 RUN git clone https://github.com/real-itu/Evocraft-py
 RUN pip install grpcio
---
+++ run_configurations/minecraft/get_the_block.py
@@ -0,0 +1,72 @@
+import ray
+import os
+import numpy as np
+from ray.rllib.agents.ppo import PPOTrainer
+from grow.env.minecraft_environment import MinecraftEnvironment
+from grow.utils.minecraft_pb2 import AIR, SEA_LANTERN, GLOWSTONE
+from grow.utils.nn import ThreeDimensionalConvolution
+from ray.rllib.models import ModelCatalog
+
+
+ModelCatalog.register_custom_model("3dconv", ThreeDimensionalConvolution)
+os.environ["CUDA_VISIBLE_DEVICES"] = "0"
+ray.init(num_gpus=1)
+
+config = {
+    "env": MinecraftEnvironment,
+    "env_config": {
+        "materials": (AIR, SEA_LANTERN),
+        "num_timestep_features": 1,
+        "max_steps": 10,
+        "reward_interval": 1,
+        "max_voxels": 6,
+        # Make sure to calibrate this with the convolutions.
+        "search_radius": 10,
+        "axiom_material": SEA_LANTERN,
+        "reward_type": "distance_from_blocks",
+        "empty_material": AIR,
+        "observing_materials": (AIR, SEA_LANTERN, GLOWSTONE),
+        "reward_block_type": GLOWSTONE,
+        "feature_type": "raw",
+        "axiom_position": None,
+    },
+    # Hypers
+    # See https://openreview.net/pdf?id=nIAxjsniDzg
+    # WHAT MATTERS FOR ON-POLICY DEEP ACTOR-CRITIC METHODS? A LARGE-SCALE STUDY
+    "model": {
+        "custom_model": "3dconv",
+        # "fcnet_hiddens": [256, 256],
+        # "fcnet_activation": "tanh",
+    },
+    "gamma": 0.99,  # One of the most important. Tune this!
+    "lr": 0.0003,  # Tune this as well.
+    "clip_param": 0.25,  # Tune: 0.1 to 0.5
+    # "vf_clip_param": 10 ** 10,  # No clipping.
+    "lambda": 0.9,  # Optimal 0.9
+    # The remaining choices were not addressed in the paper.
+    "entropy_coeff": 0.00,  # Tune?
+    # Size of batches collected from each worker.
+    # "rollout_fragment_length": 10,
+    # Number of timesteps collected for each SGD round. This defines the size
+    # of each SGD epoch.
+    # "train_batch_size": 100,
+    # Total SGD batch size across all devices for SGD. This defines the
+    # minibatch size within each epoch.
+    # "sgd_minibatch_size": 10,
+    # Settings
+    "seed": np.random.randint(2 ** 32),
+    "num_workers": 4,
+    "num_gpus": 1,
+    "num_gpus_per_worker": 0,
+    "num_envs_per_worker": 1,
+    "framework": "torch",
+    "output": "/home/ray/ray_results/escape_output",
+}
+
+ray.tune.run(
+    PPOTrainer,
+    name="get_the_block",
+    config=config,
+    checkpoint_freq=1,
+    keep_checkpoints_num=None,
+)
